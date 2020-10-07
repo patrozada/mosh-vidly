486c0c144c64d73ab467c184aacc6b2d570e8949
AzureAD\PatriciaRozada <patricia.rozada@ultimasoftware.com>
2020-10-07 11:57:38 +0200

    Add Delete button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import Table from './components/Table';
 
-
 function App() {
   return (
     <main className="container">
-    <h1>Hey there!</h1>
+      <h1 className="mt-2">My movies</h1>
       <Table />
     </main>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,34 +3,45 @@ import {getMovies} from '../services/fakeMovieService';
 
 class Table extends Component {
   state = {
-    movies: getMovies().flat()
+    movies: getMovies()
   };
-  render() {
-    console.log(this.state.movies)
-    return <table className="table">
-      <thead>
-        <tr>
-          <th> and I'm a column</th>
-          <th> and I'm a column</th>
-          <th> and I'm a column</th>
-          <th> and I'm a column</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        { this.state.movies.map(movie => 
-          <tr key={movie._id}>
-            <td> {movie.title} </td>
-            <td> {movie.genre.name}</td>
-            <td> {movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td> <button className="btn btn-danger btn-sm"> Delete </button> </td>
-          </tr>
-        )}
-      </tbody>
-      
-    </table>
 
+handleDelete = (movie) => {
+  const movies = this.state.movies.filter(m => m._id !== movie._id);
+  this.setState({movies});
+}
+
+  render() {
+    const {length: count} = this.state.movies
+    if (count === 0) {return<p>There are no movies to show</p>}
+    return (
+      <>
+        <p>There are {count} movies displayed</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th> Title</th>
+              <th> Genre</th>
+              <th> Stock</th>
+              <th> Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.movies.map(movie => 
+              <tr key={movie._id}>
+                <td> {movie.title} </td>
+                <td> {movie.genre.name}</td>
+                <td> {movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td> <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm"> Delete </button> </td>
+              </tr>
+            )}
+          </tbody>
+          
+        </table>
+      </>
+    )
   }
 }
 
