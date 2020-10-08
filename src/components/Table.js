74a487fe2f4b8c037181866445989d53b2203d6b
AzureAD\PatriciaRozada <patricia.rozada@ultimasoftware.com>
2020-10-08 12:20:36 +0200

    Add Like component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Like from './common/Like';
 import {getMovies} from '../services/fakeMovieService';
 
 class Table extends Component {
   state = {
     movies: getMovies()
   };
-
+handleLike = (movie) => {
+  const movies = [... this.state.movies];
+  const index = movies.indexOf(movie);
+  movies[index] = {...movies[index]};
+  movies[index].liked = !movies[index].liked;
+  this.setState({movies});
+}
 handleDelete = (movie) => {
   const movies = this.state.movies.filter(m => m._id !== movie._id);
   this.setState({movies});
@@ -24,6 +31,7 @@ handleDelete = (movie) => {
               <th> Genre</th>
               <th> Stock</th>
               <th> Rate</th>
+              <th>Like</th>
               <th></th>
             </tr>
           </thead>
@@ -34,6 +42,7 @@ handleDelete = (movie) => {
                 <td> {movie.genre.name}</td>
                 <td> {movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
                 <td> <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm"> Delete </button> </td>
               </tr>
             )}
