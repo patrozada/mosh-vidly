f65cbcbf92e93a508be393ab95aaf085672c9c7c
AzureAD\PatriciaRozada <patricia.rozada@ultimasoftware.com>
2020-10-08 13:15:09 +0200

    Add pagination component. Add lodash

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Like from './common/Like';
 import {getMovies} from '../services/fakeMovieService';
+import Pagination from './common/Pagination'
 
 class Table extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 handleLike = (movie) => {
-  const movies = [... this.state.movies];
+  const movies = [...this.state.movies];
   const index = movies.indexOf(movie);
   movies[index] = {...movies[index]};
   movies[index].liked = !movies[index].liked;
@@ -17,9 +20,12 @@ handleDelete = (movie) => {
   const movies = this.state.movies.filter(m => m._id !== movie._id);
   this.setState({movies});
 }
-
+handlePageChange = (page) => {
+  this.setState({currentPage: page})
+}
   render() {
-    const {length: count} = this.state.movies
+    const {length: count} = this.state.movies;
+    const { currentPage, pageSize } = this.state;
     if (count === 0) {return<p>There are no movies to show</p>}
     return (
       <>
@@ -47,7 +53,12 @@ handleDelete = (movie) => {
               </tr>
             )}
           </tbody>
-          
+          <Pagination 
+            itemsCount={count} 
+            onPageChange={this.handlePageChange} 
+            pageSize={pageSize}
+            currentPage={currentPage}
+          />
         </table>
       </>
     )
---
+++ src/components/common/Pagination.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import _ from 'lodash';
+
+const Pagination = props => {
+  const {itemsCount, pageSize, onPageChange, currentPage } = props;
+  const pagesCount = Math.ceil(itemsCount/pageSize);
+  const pages = _.range(1, pagesCount + 1);
+  if (pagesCount === 1) return null;
+  return (
+      <ul className="pagination">
+        {pages.map( page => (
+           <li key={page} className={ page === currentPage ? "page-item active" : "page-item"}><a onClick={() => onPageChange(page)}className="page-link" >{page}</a></li>
+        ))}
+      </ul>
+    );
+  
+}
+ 
+export default Pagination;
